865d3e023cbc27a1bb6502d665a8907407430cab
Miroslaw Opoka <miroslaw@opoka.tech>
2024-02-29 22:47:42 +0100

    Time_Point tests

--- tests/Time_Point_Tests.cpp
+++ tests/Time_Point_Tests.cpp
@@ -2,12 +2,45 @@
 
 #include "Time_Point.hpp"
 
+#include <vector>
+
 using namespace ECB;
 
 TEST(Time_Point, Assigning_Invalid_Data_Results_In_Empty_Time_Point)
 {
-    Time_Point tp{""};
-    EXPECT_FALSE(tp.is_initialized());
+    // clang-format off
+    const std::vector<std::string> invalid_strings = {
+        "",
+        "2020-01-01 01:00:00",
+        "2020-01-01-01",
+        "2020-01-01-01-01",
+        "2020-0",
+        "1234-56_78",
+        "yyyy-mm-dd"};
+    // clang-format on
 
-    EXPECT_EQ(std::string{""}, static_cast<std::string>(tp));
+    const std::string empty_string;
+    for (const auto &inv: invalid_strings)
+    {
+        Time_Point tp{inv};
+        EXPECT_FALSE(tp.is_initialized());
+        EXPECT_EQ(empty_string, static_cast<std::string>(tp));
+    }
+}
+
+TEST(Time_Point, Assigning_Valid_Data_Results_In_Non_Empty_Time_Point)
+{
+    // clang-format off
+    const std::vector<std::string> valid_strings = {
+        "2020-01-01",
+        "1234-56-78", // no check is made regarding the date
+        "2020-02-29"};
+    // clang-format on
+
+    for (const auto &inv: valid_strings)
+    {
+        Time_Point tp{inv};
+        EXPECT_TRUE(tp.is_initialized());
+        EXPECT_EQ(inv, static_cast<std::string>(tp));
+    }
 }
